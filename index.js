2f088e61be2780083568944895725340723145ba
CallyCa <blackyrite@hotmail.com>
2020-09-08 13:08:33 -0300

    Criacao das pag html e criando um metodo post no nodejs

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+
+app.get("/",(req,res) => {
+    res.render("index");
+});
+
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar");
+})
+
+app.post("/salvarpergunta", (req,res) =>{
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("formulario recebido! titulo " + titulo + " " + "descricao " + descricao);
+
+});
+
+app.listen(3000,() =>{
+    console.log("app rodando em http://localhost:3000");
+});
